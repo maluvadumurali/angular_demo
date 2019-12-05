37ce0d69d1a08b7fcbaddfd257487a8efbf3fca9
Murali-MK <muralimk@outlook.com>
2019-12-06 00:18:19 +0530

    mat cards added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  news = new Array(20);
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
